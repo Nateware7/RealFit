de67c4697a93d2d79919e81ecb6b6ba4c5451ec1
unknown <nateware777@gmail.com>
2024-11-10 23:58:19 +0300

    Added a Post page & Edit Profile Page

--- routes/main.js
+++ routes/main.js
@@ -8,7 +8,9 @@ const { ensureAuth, ensureGuest } = require("../middleware/auth");
 //Main Routes - simplified for now
 router.get("/", homeController.getIndex);
 router.get("/profile", ensureAuth, postsController.getProfile);
+router.get("/add", ensureAuth, postsController.getAdd);
 router.get("/feed", ensureAuth, postsController.getFeed);
+router.get("/edit", ensureAuth, postsController.getEdit);
 router.get("/login", authController.getLogin);
 router.post("/login", authController.postLogin);
 router.get("/logout", authController.logout);

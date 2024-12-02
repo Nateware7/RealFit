3c82284e27a993e1b136599b35d06f9d006cc264
unknown <nateware777@gmail.com>
2024-12-02 12:09:09 +0300

    User Can Delete Comment

--- controllers/comments.js
+++ controllers/comments.js
@@ -16,9 +16,33 @@ module.exports = {
       await Post.findByIdAndUpdate(req.params.id, { $inc: { comments: 1 } });
 
       console.log("Comment has been added and post's comment count updated!");
-      res.redirect("/post/" + req.params.id);
+      res.redirect(`/post/${req.params.id}`);
     } catch (err) {
       console.log(err);
+      res.redirect(`/post/${req.params.id}`);
     }
   },
-};
+
+  deleteComment: async (req, res) => {
+    try {
+      const comment = await Comment.findById(req.params.commentId);
+
+      // Check if the logged-in user is the owner of the comment
+      if (comment.user.toString() !== req.user.id.toString()) {
+        return res.redirect(`/post/${comment.post}`);
+      }
+
+      // Delete the comment
+      await comment.remove();
+      console.log("Deleted Comment");
+
+      // Decrement the comment count in the Post model
+      await Post.findByIdAndUpdate(comment.post, { $inc: { comments: -1 } });
+
+      res.redirect(`/post/${comment.post}`);
+    } catch (err) {
+      console.log(err);
+      res.redirect(`/post/${req.params.id}`);
+    }
+  },
+}
--- routes/comments.js
+++ routes/comments.js
@@ -5,5 +5,8 @@ const { ensureAuth } = require("../middleware/auth");
 
 //Comment Routes - simplified for now
 router.post("/createComment/:id", ensureAuth, commentsController.createComment);
+// In routes/comments.js
+router.post("/deleteComment/:commentId", ensureAuth, commentsController.deleteComment);
+
 
 module.exports = router;
